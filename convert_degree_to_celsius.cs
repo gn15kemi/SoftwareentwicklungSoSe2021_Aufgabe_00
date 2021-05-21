28d1a25dab0d6c980b8dd3f4e50295ae7a1f822e
lorell <lorelltchapda@gmail.com>
2021-05-21 14:21:55 +0200

    temperatur convertion

---
+++ convert_degree_to_celsius.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace aufgabe00_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float celsius, fahrenheit;
+            celsius = 5;
+            fahrenheit = ((9 * celsius) / 5) + 32;
+            Console.WriteLine("{0} Celsius entspricht {1} Fahrenheit", celsius, fahrenheit);
+            Console.WriteLine("Celsius: ");
+            celsius = float.Parse(Console.ReadLine());
+            fahrenheit = ((9 * celsius) / 5) + 32;
+            Console.WriteLine("Fahrenheit:{0} ", fahrenheit);
+        }
+    }
+}
